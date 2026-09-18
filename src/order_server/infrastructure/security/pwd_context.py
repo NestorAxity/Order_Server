25d20588602e4ec0ce947fc1d9f8c6c7561c4664
Nestor Hernandez <nestor.hernandezc@axity.com>
2026-09-17 23:50:42 -0600

    (auth): Correccion mypy
    correccion de cast redundantes
    
    Date: 17/09/2026

--- src/order_server/infrastructure/security/pwd_context.py
+++ src/order_server/infrastructure/security/pwd_context.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta, timezone
-from typing import Any, cast
+from typing import Any
 
 import jwt
 from pwdlib import PasswordHash
@@ -12,11 +12,11 @@ password_hash = PasswordHash((BcryptHasher(),))
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return cast(bool, password_hash.verify(plain_password, hashed_password))
+    return bool, password_hash.verify(plain_password, hashed_password)
 
 
 def get_password_hash(password: str) -> str:
-    return cast(str, password_hash.hash(password))
+    return str, password_hash.hash(password)
 
 
 def create_access_token(
@@ -40,4 +40,4 @@ def create_access_token(
         settings.SECRET_KEY,
         algorithm=settings.ALGORITHM,
     )
-    return cast(str, encoded_jwt)
+    return str, encoded_jwt
